3029fffeca854edf88aa921b49757fe128d82c6d
julyannamenezes <menezesjulyanna@gmail.com>
2025-07-19 09:32:52 -0300

    Add files via upload

---
+++ Desafio01.py
@@ -0,0 +1,11 @@
+# Verificador de Número Par oi Ímpar:
+#Crie um programa que verifique se um número (informado pelo usuário) é par ou ímpar
+
+#retornar uma string (texto)
+#o retorno dentro de uma variável
+
+numero = int(input('Digite um número: '))
+if (numero %2) == 0:
+    print(f' O {numero} é par!')
+else:
+    print(f' O {numero} é ímpar!')
