ea36823ddb612ef56b07b30bb0cabbd601def260
julyannamenezes <menezesjulyanna@gmail.com>
2025-08-02 11:41:31 -0300

    .

---
+++ aula03/exemplo03.py
@@ -0,0 +1,6 @@
+posgraduandos = ['Geisiane', 'Eduardo', 'Maria Augusta', 'George', 'Newton', 'Raysse', 'Isabela']
+
+mensagem = ', venha para minha festa de aniversário, mas traga a sua comida e o seu vinho! (Att. Agda)'
+
+for nome in posgraduandos:
+    print(nome + mensagem)
